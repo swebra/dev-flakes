4bfc52f726f46adc4dd40282f35135c058cde8d4
Eric Claerhout <eric.claerhout@drivewyze.com>
2025-01-08 15:12:27 +0000

    Add otto project

---
+++ otto/flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "A development flake for otto";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+  };
+
+  outputs = {nixpkgs, ...}: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells.${system}.default = pkgs.mkShellNoCC {
+      venvDir = ".venv";
+      packages = with pkgs; [
+        nodejs
+        corepack
+      ];
+    };
+  };
+}
