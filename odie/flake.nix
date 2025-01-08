4deb89ed5b287c6bec366810a33aaf60786c11fa
swebra <eric@swebra.com>
2025-01-08 15:12:19 +0000

    Add odie project

---
+++ odie/flake.nix
@@ -0,0 +1,37 @@
+{
+  description = ''
+    A development flake for ODIE where Python packages can be installed through pip from a
+    traditional requirements.txt rather than being managed through nix.
+  '';
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+  };
+
+  outputs = {nixpkgs, ...}: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    # mkShellNoCC not applicable as pip packages require C compiling
+    devShells.${system}.default = pkgs.mkShell {
+      venvDir = ".venv";
+      packages = with pkgs.python313Packages; [
+        python
+        pip
+        venvShellHook
+      ];
+
+      # Below all provides build and runtime dependencies for pip-installed packages
+
+      buildInputs = with pkgs; [
+        postgresql_15 # Provides libpg for building psycopg
+      ];
+
+      LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+        pkgs.stdenv.cc.cc
+        pkgs.gdal
+        pkgs.geos
+      ];
+    };
+  };
+}
