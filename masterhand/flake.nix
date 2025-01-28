b7e848f2e8ca1e498b5cf283e6575ed0af5f7c23
swebra <eric@swebra.com>
2025-01-28 17:44:06 +0000

    Add masterhand project

---
+++ masterhand/flake.nix
@@ -0,0 +1,35 @@
+{
+  description = ''
+    A development flake for master where Python packages can be installed through pip from a
+    traditional requirements.txt rather than being managed through nix.
+  '';
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+  };
+
+  outputs = {nixpkgs, ...}: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    # mkShellNoCC not applicable as psycopg requires C compiling
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs.python311Packages; [
+        python
+        pip
+        venvShellHook
+      ];
+
+      venvDir = ".venv";
+      postVenvCreation = "pip install -r requirements.txt";
+
+      # Below all provides build and runtime dependencies for pip-installed packages
+
+      buildInputs = with pkgs; [
+        postgresql_15 # Provides libpg for building psycopg
+      ];
+
+      LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [pkgs.stdenv.cc.cc];
+    };
+  };
+}
