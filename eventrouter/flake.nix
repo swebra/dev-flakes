2ab7c43d8972f1d08c1182cd04548ec654ae4805
swebra <eric@swebra.com>
2025-11-04 17:23:54 +0000

    Add eventrouter project

---
+++ eventrouter/flake.nix
@@ -0,0 +1,30 @@
+{
+  description = ''
+    A development flake for eventrouter where Python packages can be installed through
+    pip from a traditional requirements.txt rather than being managed through nix.
+  '';
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+  };
+
+  outputs = {nixpkgs, ...}: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells.${system}.default = pkgs.mkShellNoCC {
+      packages = with pkgs.python311Packages; [
+        python
+        pip
+        venvShellHook
+      ];
+
+      venvDir = ".venv";
+      postVenvCreation = "pip install -r requirements.txt";
+
+      # Below all provides build and runtime dependencies for pip-installed packages
+
+      env.LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [pkgs.stdenv.cc.cc.lib];
+    };
+  };
+}
